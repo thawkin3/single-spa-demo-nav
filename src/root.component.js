a8a928d8cfa2e7569fd00a87fe59f29ce765d6ea
Tyler Hawkins <tylerhawkins@workfront.com>
2020-06-01 21:00:38 -0600

    adds react router

--- src/root.component.js
+++ src/root.component.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./root.component.css";
 
 export default function Root() {
   return (
-    <nav className="nav">
-      <a href="/page1" className="link">
-        Page 1
-      </a>
-      <a href="/page2" className="link">
-        Page 2
-      </a>
-    </nav>
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/page1" className="link">
+          Page 1
+        </Link>
+        <Link to="/page2" className="link">
+          Page 2
+        </Link>
+      </nav>
+    </BrowserRouter>
   );
 }
